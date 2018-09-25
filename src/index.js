355ee77c00f4ec9321e544f45ec3c50380be3293
Marina Avila <marina@grama.cc>
2018-09-25 10:29:13 -0300

    fist test

--- src/index.js
+++ src/index.js
@@ -5,10 +5,28 @@ import './scss/style.scss';
 
 const setup = async () => {
   const data = await $.getJSON('data.json')
-  console.log(data.batata)
-  $('#header').append(`
-    <div class="teste29"></div>
+  const content = $('#content');
+
+  console.log(data.economics.answers.yes)
+
+  content.append(`
+    <div class="teste29">${data.economics.question}</div>
+    <div class="teste29">${data.economics.answers}</div>
+
   `)
+
+  data.economics.answers.forEach((answer, index) => {
+    content.append(`
+      <div>batata</div>
+    `)
+  })
+
+  data.economics.answers.yes.forEach((yes, index) => {
+    content.append(`
+      <div>${yes.ciro}</div>
+    `)
+  })
+
 }
 
 setup();
